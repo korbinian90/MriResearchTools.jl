ef882ceb1353716b1a80776c8fcdd98ed945a754
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2019-02-13 18:43:15 +0100

    other unwrapping methods added

--- src/MRI.jl
+++ src/MRI.jl
@@ -4,6 +4,8 @@ using Statistics
 
 include("NIfTI_mod.jl")
 include("Utility.jl")
+include("hussein3d.jl")
+include("laplacianunwrapping.jl")
 
 export Data,
         readphase,
@@ -13,7 +15,9 @@ export Data,
         NIVolume,
         niread,
         niwrite,
-        write_emptynii
+        write_emptynii,
+        hussein3d, hussein3d!,
+        laplacianunwrap, laplacianunwrap!
 
 
 end # module
