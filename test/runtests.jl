58ee4fada8200d367f83fb422aa5fd3ece05f2c7
korbi <korbinian90@gmail.com>
2020-04-06 15:24:26 +0200

    renaming tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using MriResearchTools
 using Test
 
 @testset "MriResearchTools.jl" begin
-    include("unwrapping_test.jl")
-    include("utility_test.jl")
-    include("intensitycorrection_test.jl")
+    include("unwrapping.jl")
+    include("utility.jl")
+    include("intensitycorrection.jl")
 end
