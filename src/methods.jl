873773a46e05714bf98c541509219b3753f4d9f9
korbinian90 <korbinian.eckstein@gmail.com>
2020-09-21 09:47:43 +0200

    refactor calculateB0

--- src/methods.jl
+++ src/methods.jl
@@ -3,6 +3,5 @@ homodyne(I; kwargs...) = homodyne!(copy(I); kwargs...)
 homodyne!(I; dims=1:2, σ=8 .* ones(length(dims))) = I ./= gaussiansmooth3d(I, σ; dims=dims)
 
 function calculateB0_unwrapped(unwrapped_phase, mag, TEs)
-    TEs = reshape(TEs,1,1,1,:)
-    return (1000 / 2π) * sum(unwrapped_phase .* mag; dims=4) ./ sum(mag .* TEs; dims=4) 
+    return (1000 / 2π) * sum(unwrapped_phase .* mag; dims=4) ./ sum(mag .* to_dim(TEs,4); dims=4)
 end
