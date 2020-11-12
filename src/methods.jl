4ed799fc1e6b98244dc90394d034f636faccce62
korbinian90 <korbinian.eckstein@gmail.com>
2020-11-12 07:34:10 +0100

    dropdims in calculateB0

--- src/methods.jl
+++ src/methods.jl
@@ -4,5 +4,6 @@ homodyne!(I; dims=1:2, σ=8 .* ones(length(dims))) = I ./= gaussiansmooth3d(I, �
 
 # calculates B0 in [Hz]
 function calculateB0_unwrapped(unwrapped_phase, mag, TEs)
-    return (1000 / 2π) * sum(unwrapped_phase .* mag; dims=4) ./ sum(mag .* to_dim(TEs,4); dims=4)
+    dims=4
+    return (1000 / 2π) * sum(unwrapped_phase .* mag; dims=dims) ./ sum(mag .* to_dim(TEs,4); dims=dims) |> I -> dropdims(I; dims=dims)
 end
