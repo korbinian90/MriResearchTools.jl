4d5a377bb18ea636eabef590da5ce13a6cad94ea
Korbinian Eckstein <korbinian90@gmail.com>
2022-06-29 00:35:57 +1000

    move voxelquality to ROMEO.jl

--- src/romeofunctions.jl
+++ src/romeofunctions.jl
@@ -1,16 +1,6 @@
 const romeo = unwrap # access unwrap function via alias romeo
 const romeo! = unwrap!
 
-function ROMEO.calculateweights(phase::AbstractArray{T,4}; TEs, template=2, p2ref=1, keyargs...) where T
-    args = Dict{Symbol, Any}(keyargs)
-    args[:phase2] = phase[:,:,:,p2ref]
-    args[:TEs] = TEs[[template, p2ref]]
-    if haskey(args, :mag) && size(args[:mag], 4) > 1
-        args[:mag] = args[:mag][:,:,:,template]
-    end
-    return ROMEO.calculateweights(view(phase,:,:,:,template); args...)
-end
-
 """
     calculateB0_unwrapped(unwrapped_phase, mag, TEs)
 
@@ -28,27 +18,4 @@ function calculateB0_unwrapped(unwrapped_phase, mag, TEs)
     return B0
 end
 
-"""
-    romeovoxelquality(phase::AbstractArray; keyargs...)
-
-Calculates a quality for each voxel. The voxel quality can be used to create a mask.
-
-# Examples
-```julia-repl
-julia> qmap = romeovoxelquality(phase_3echo; TEs=[1,2,3]);
-julia> mask = robustmask(qmap);
-```
-     
-Takes the same inputs as `romeo`/`unwrap`:
-$(@doc unwrap)
-
-See also [`romeo`](@ref), [`robustmask`](@ref), [`brain_mask`](@ref), [`phase_based_mask`](@ref)
-""" 
-function romeovoxelquality(phase; keyargs...)
-    weights = ROMEO.calculateweights(phase; type=Float32, rescale=x->x, keyargs...) # [0;1]
-    qmap = dropdims(sum(weights; dims=1); dims=1)
-    qmap[2:end,:,:] .+= weights[1,1:end-1,:,:]
-    qmap[:,2:end,:] .+= weights[2,:,1:end-1,:]
-    qmap[:,:,2:end] .+= weights[3,:,:,1:end-1]
-    return qmap ./ 6 # [0;1]
-end
+romeovoxelquality = voxelquality
