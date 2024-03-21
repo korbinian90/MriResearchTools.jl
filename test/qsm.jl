e816752665fb424754f06a10e2c5e7da46b6f020
Korbinian Eckstein <korbinian90@gmail.com>
2024-03-21 13:58:49 +0100

    test-fix: reduce iterations in a way that works for rts-qsm as well

--- test/qsm.jl
+++ test/qsm.jl
@@ -11,7 +11,7 @@ phase_nii = readphase(fn_phase)
 mag_nii = readmag(fn_mag)
 TEs = 4:4:12
 
-vsz = header(phase_nii).pixdim[2:4]
+vsz = header(phase_nii).pixdim[2:4] .* 10 # reduces required iterations for testing
 phase = Float32.(phase_nii)
 mag = Float32.(mag_nii)
 mask = qsm_mask_filled(phase[:,:,:,1])
@@ -23,13 +23,13 @@ args = (phase, mag, mask, TEs, vsz)
 
 
 # QSM multi-echo postaverage (inverse-variance-weighted averaging)
-qsm_laplacian_average = qsm_average(args...; B0, iterations=100)
+qsm_laplacian_average = qsm_average(args...; B0)
 # QSM.jl
 # qsm_laplacian_average = qsm_average(args...; B0, unwrapping=laplacianunwrap)
 # qsm_romeo_average = qsm_average(args...; B0, unwrapping=romeo)
 
 # QSM multi-echo phase combine
-qsm_laplacian_combined = qsm_laplacian_combine(args...; B0, iterations=100)
-qsm_romeo_B0_map = qsm_romeo_B0(args...; B0, iterations=100)
+qsm_laplacian_combined = qsm_laplacian_combine(args...; B0)
+qsm_romeo_B0_map = qsm_romeo_B0(args...; B0)
 
 end
