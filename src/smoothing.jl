65be389636b0b6701f00dbe6ba3d8094c5a19cbc
Korbinian Eckstein <korbinian90@gmail.com>
2024-03-21 11:34:08 +0100

    add padding to the gaussiansmooth3d documentation

--- src/smoothing.jl
+++ src/smoothing.jl
@@ -5,7 +5,8 @@
         mask=nothing,
         nbox=ifelse(isnothing(mask), 3, 6), 
         weight=nothing, dims=1:min(ndims(image),3), 
-        boxsizes=getboxsizes.(sigma, nbox)
+        boxsizes=getboxsizes.(sigma, nbox),
+        padding=false
         )
 
 Performs Gaussian smoothing on `image` with `sigma` as standard deviation of the Gaussian.
@@ -18,6 +19,7 @@ Optional arguments:
 - `weight`: Apply weighted smoothing. Either weighted or masked smoothing can be porformed.
 - `dims`: Specify which dims should be smoothed. Corresponds to manually looping of the other dimensions.
 - `boxizes`: Manually specify the boxsizes, not using the provided sigma. `length(boxsizes)==length(dims) && length(boxsizes[1])==nbox`
+- `padding`: If `true`, the image is padded with zeros before smoothing and the padding is removed afterwards.
 """
 gaussiansmooth3d, gaussiansmooth3d!
 
